0da2025cbbe34e6bcf75cc5a49d06209c78440a5
James Mohr <jamesM2@pragmaworld.net>
2023-04-10 16:17:09 +0200

    WIP

--- Pump.Android/MainActivity.cs
+++ Pump.Android/MainActivity.cs
@@ -4,19 +4,23 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using System.Collections.Generic;
+using Xamarin.Essentials;
 
 namespace Pump.Droid
 {
     [Activity(Label = "Pump", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override async void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Rg.Plugins.Popup.Popup.Init(this);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
             LoadApplication(new App());
+            await Permissions.RequestAsync<BLEPermission>();
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
@@ -32,4 +36,15 @@ namespace Pump.Droid
         }
  
     }
+
+    public class BLEPermission : Xamarin.Essentials.Permissions.BasePlatformPermission
+    {
+        public override (string androidPermission, bool isRuntime)[] RequiredPermissions => new List<(string androidPermission, bool isRuntime)>
+        {
+            (Android.Manifest.Permission.BluetoothScan, true),
+            (Android.Manifest.Permission.BluetoothConnect, true),
+            (Android.Manifest.Permission.AccessFineLocation, true)
+         }.ToArray();
+    }
+
 }
